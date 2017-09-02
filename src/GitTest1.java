f83eb136733ec0e99459e574dede3da4224872d5
sisseo <31310631+sisseo@users.noreply.github.com>
2017-09-02 11:40:04 +0900

    Update GitTest1.java

--- src/GitTest1.java
+++ src/GitTest1.java
@@ -4,7 +4,8 @@ public class GitTest1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world");
-		System.out.println("hello world");
+		
+		
 
 	}
 
